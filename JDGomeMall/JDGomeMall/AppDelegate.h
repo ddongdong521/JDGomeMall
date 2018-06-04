1b046afed7e2573c56d70e140697cb8d4821c64e
邓建东 <595888098@qq.com>
2018-06-04 17:48:23 +0800

    创建项目

---
+++ JDGomeMall/JDGomeMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDGomeMall
+//
+//  Created by 邓建东 on 2018/6/4.
+//  Copyright © 2018年 邓建东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDGomeMall/JDGomeMall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JDGomeMall
+//
+//  Created by 邓建东 on 2018/6/4.
+//  Copyright © 2018年 邓建东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
